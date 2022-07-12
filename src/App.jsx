464b194fed666a4a7061f602fa31255f32740787
Vivahjah <mengwa44@yahoo.com>
2022-07-12 02:16:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Brand, Cta, Article, Features, Navbar} from './components'
+
+const App = () => {
+  return (
+    <>
+      <h1>GTA</h1>
+    </>
+  );
+};
+
+export default App;
